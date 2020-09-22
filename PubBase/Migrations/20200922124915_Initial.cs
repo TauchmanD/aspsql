9daf729c947ddccbf01ac48fe329d1c5e836ce7e
Denis Tauchman <denis.tauchman@pslib.cz>
2020-09-22 14:52:30 +0200

    rdy

---
+++ PubBase/Migrations/20200922124915_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PubBase.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Municipality = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pubs", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pubs",
+                columns: new[] { "Id", "Municipality", "Name" },
+                values: new object[] { 1, "Liberec", "Svah" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pubs");
+        }
+    }
+}
--- PubBase/Models/AppDbContext.cs
+++ PubBase/Models/AppDbContext.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PubBase.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Pub> Pubs { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Pub>().HasData(
+                new Pub { Id = 1, Name = "Svah", Municipality = "Liberec" });
+                new Pub { Id = 2, Name = "Techtle", Municipality = "Jablonec nad Jablonec" };
+        }
     }
 }
